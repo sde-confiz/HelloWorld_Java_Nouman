3a17c2363bebae0fb3bbd73bf8d7123d1a3c3924
Nouman Khalid <nouman.khalid@confiz.com>
2019-01-08 19:08:05 +0500

    First Commit

---
+++ src/main/java/com/confiz/sde_support/onboarding/java/nouman/App.java
@@ -0,0 +1,13 @@
+package com.confiz.sde_support.onboarding.java.nouman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/confiz/sde_support/onboarding/java/nouman/AppTest.java
@@ -0,0 +1,20 @@
+package com.confiz.sde_support.onboarding.java.nouman;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
